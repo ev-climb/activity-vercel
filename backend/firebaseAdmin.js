6817b67a0721d59af7b843299933cc599436f00e
evseev <evgeny.evseev@pimpay.ru>
2025-05-21 15:07:31 +0300

    updated package

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,11 +1,12 @@
 // firebaseAdmin.js
 import admin from 'firebase-admin';
-import serviceAccount from './serviceAccountKey.json' assert { type: 'json' };
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
-  });
+  })
 }
 
 const db = admin.firestore();
