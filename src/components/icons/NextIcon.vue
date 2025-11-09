72ce0e401ee1b04ae89ce95ee22fcf961666afbc
evseev <evgeny.evseev@pimpay.ru>
2025-11-09 20:16:40 +0300

    Fix icon

--- src/components/icons/NextIcon.vue
+++ src/components/icons/NextIcon.vue
@@ -1,5 +1,5 @@
 <template>
-<svg fill="currentColor" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+<svg fill="currentColor" width="100%" height="100%" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 
 <g id="SVGRepo_bgCarrier" stroke-width="0"/>
 
